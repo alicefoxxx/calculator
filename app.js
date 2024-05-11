b3f65cf9a8420ffd74140f3f64d42dae034e460e
alicefoxxx <mumuttemy@nate.com>
2024-05-11 16:49:24 +0900

    feature: Add minus

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// 폼에서 값 추출
-const firstValue = document.getElementById("first");
-const secondValue = document.getElementById("second");
+const addForm = document.getElementById("add-form");
+const subForm = document.getElementById("sub-form");
 
-const output = document.getElementById("result");
-
-// 결과값 출력
+// 덧셈
 const formSubmitHandler = (event) => {
   event.preventDefault();
+
+  const firstValue = document.getElementById("first");
+  const secondValue = document.getElementById("second");
+
+  const output = document.getElementById("result");
 
   // 덧셈 함수 호출
   const result = add(Number(firstValue.value), Number(secondValue.value));
@@ -15,7 +17,29 @@ const formSubmitHandler = (event) => {
   output.textContent = result;
 };
 
-form.addEventListener("submit", formSubmitHandler);
+// 뺄셈
+const minusFormSubmitHandler = (event) => {
+  event.preventDefault();
+
+  const minusFirstValue = document.getElementById("minusFirst");
+  const minusSecondValue = document.getElementById("minusSecond");
+  const minusOutput = document.getElementById("minusResult");
+
+  // 뺄셈 함수 호출
+  const result = minus(
+    Number(minusFirstValue.value),
+    Number(minusSecondValue.value)
+  );
+
+  // 결과 값 출력
+  minusOutput.textContent = result;
+};
+
+addForm.addEventListener("submit", formSubmitHandler);
+subForm.addEventListener("submit", minusFormSubmitHandler);
 
 // 덧셈 함수 구현
 const add = (a, b) => a + b;
+
+// 마이너스 함수 구현
+const minus = (a, b) => a - b;
